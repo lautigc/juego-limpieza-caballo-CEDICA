c018c3c450a9d9dcf0c501f1388298aeb9668d1d
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-01-31 00:47:30 -0300

    Se mejora la logica para el manejo de sesion

--- app/src/main/java/com/cedica/cedica/core/session/session.kt
+++ app/src/main/java/com/cedica/cedica/core/session/session.kt
@@ -3,31 +3,42 @@ package com.cedica.cedica.core.session
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class SessionRepository(
-    private val dataStore: DataStore<Preferences>
-){
-    private companion object {
-        val USER_ID = intPreferencesKey("USER_ID")
+// Definición de datastore
+private const val DATA_STORE_NAME = "SESSION"
+val Context.sessionStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
+
+/*
+* Session object to management user session
+ */
+object Session {
+    // DataStore must initialize in root UI
+    private lateinit var dataStore: DataStore<Preferences>
+
+    /*
+    * Initialize Singleton object. If you don't call this method, lateinit exception will be thrown
+     */
+    fun init(dataStore: DataStore<Preferences>) {
+        this.dataStore = dataStore
     }
 
-    suspend fun saveUserID(userID: Int) {
-        dataStore.edit { preferences ->
+    // Key-values for datastore
+    private val USER_ID = intPreferencesKey("USER_ID")
+
+    suspend fun setUserID(userID: Int) {
+        this.dataStore.edit { preferences ->
             preferences[USER_ID] = userID
         }
     }
 
-    val userID: Flow<Int?> = dataStore.data.map { preferences ->
-        preferences[USER_ID]
+    fun getUserID(): Flow<Int?> {
+        return this.dataStore.data.map { preferences ->
+            preferences[USER_ID]
+        }
     }
 }
-
-// Definición del data store
-const val DATA_STORE_NAME = "SESSION"
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DATA_STORE_NAME)
