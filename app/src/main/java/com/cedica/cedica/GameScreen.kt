102079c0ec125ad6492416682f660dd9bc054589
Franco Kumichel <francokumichel1996@gmail.com>
2025-02-04 19:01:46 -0300

    Agrego funcionalidad para mostrar mensajes referentes a la selección de herramientas y parte del caballo

--- app/src/main/java/com/cedica/cedica/GameScreen.kt
+++ app/src/main/java/com/cedica/cedica/GameScreen.kt
@@ -15,10 +15,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -34,19 +41,19 @@ fun GameScreen(navigateToMenu: () -> Unit) {
 
     var selectedTool by remember { mutableStateOf<Int?>(null) }
     var selectedHorsePart by remember { mutableStateOf(false) }
-    val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
     var offsetX by remember { mutableFloatStateOf(-150f) }
     var offsetY by remember { mutableFloatStateOf(-150f) }
     var isDraggable by remember { mutableStateOf(false) }
+    var messageType by remember { mutableStateOf("selection") }
+    var customMessage by remember { mutableStateOf<String?>(null) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFFFE4B5))
     ) {
-        SnackbarHost(hostState = snackbarHostState, modifier = Modifier.align(Alignment.TopCenter))
 
         Box(
             modifier = Modifier
@@ -54,11 +61,19 @@ fun GameScreen(navigateToMenu: () -> Unit) {
                 .padding(16.dp),
             contentAlignment = Alignment.TopStart
         ) {
-            Text(
-                text = "Volver al Menú",
-                color = Color.Blue,
-                modifier = Modifier.clickable { navigateToMenu() }
-            )
+            Button(onClick = { navigateToMenu() }, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFADD8E6))){
+                Text("Volver al menú", color = Color.Black)
+            }
+
+            if (messageType.isNotEmpty()) {
+                MessageBox(
+                    messageType = messageType,
+                    customMessage = customMessage,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                )
+            }
+
         }
 
         Column(
@@ -77,16 +92,17 @@ fun GameScreen(navigateToMenu: () -> Unit) {
                     painter = painterResource(R.drawable.caballo),
                     contentDescription = "Caballo",
                     modifier = Modifier
-                        .size(350.dp)
+                        .size(400.dp)
                         .border(
                             width = if (selectedHorsePart) 4.dp else 0.dp,
-                            color = if (selectedHorsePart) Color.Green else Color.Transparent
+                            color = if (selectedHorsePart) Color.Black else Color.Transparent
                         )
                         .clickable {
                             selectedHorsePart = true
                             isDraggable = true
+                            customMessage = "¡Excelente! Seleccionaste la parte correcta del caballo"
                             coroutineScope.launch {
-                                snackbarHostState.showSnackbar("Parte del caballo seleccionada correctamente")
+                                messageType = "success"
                             }
                         }
                 )
@@ -110,10 +126,11 @@ fun GameScreen(navigateToMenu: () -> Unit) {
                         Image(
                             painter = painterResource(tool),
                             contentDescription = "Herramienta arrastrable",
-                            modifier = Modifier.size(100.dp)
+                            modifier = Modifier.size(60.dp)
                         )
                     }
                 }
+
             }
 
             Spacer(modifier = Modifier.weight(1f))
@@ -121,7 +138,7 @@ fun GameScreen(navigateToMenu: () -> Unit) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(120.dp),
+                    .height(85.dp),
                 contentAlignment = Alignment.Center
             ) {
                 ImageSelectionList(
@@ -131,7 +148,10 @@ fun GameScreen(navigateToMenu: () -> Unit) {
                         if (selectedTool == null) {
                             selectedTool = tool
                             coroutineScope.launch {
-                                snackbarHostState.showSnackbar("Herramienta seleccionada correctamente")
+                                messageType = "success"
+                                delay(5000)
+                                customMessage = "¿Qué parte del caballo hay que limpiar ahora?"
+                                messageType = "selection"
                             }
                         }
                     }
@@ -149,8 +169,8 @@ fun ImageSelectionList(
     modifier: Modifier = Modifier
 ) {
     LazyRow(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
     ) {
         items(images) { imageRes ->
             SelectableImage(
@@ -170,15 +190,54 @@ fun SelectableImage(imageRes: Int, isSelected: Boolean, onClick: () -> Unit) {
             .padding(8.dp)
             .border(
                 width = if (isSelected) 4.dp else 2.dp,
-                color = if (isSelected) Color.Blue else Color.Black
+                color = if (isSelected) Color(0xFFADD8E6) else Color.Black,
+                RoundedCornerShape(16.dp)
             )
             .clickable { onClick() },
-        contentAlignment = Alignment.BottomStart
+        contentAlignment = Alignment.Center
     ) {
         Image(
             painter = painterResource(id = imageRes),
             contentDescription = "Imagen seleccionable",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(70.dp),
+            alignment = Alignment.Center
+        )
+    }
+}
+
+@Composable
+fun MessageBox(messageType: String, customMessage: String? = null, modifier: Modifier = Modifier) {
+    val (message, image) = when (messageType) {
+        "selection" -> Pair(customMessage ?: "¿Qué herramienta hay que seleccionar ahora?", R.drawable.vault_boy_thinking)
+        "error" -> Pair(customMessage ?: "Ups... la herramienta seleccionada no es la correcta.", R.drawable.vault_boy_thumbs_down)
+        "success" -> Pair(customMessage ?: "¡Perfecto! Has seleccionado la herramienta correcta", R.drawable.vault_boy_thumbs_up)
+        else -> Pair("", 0)
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth(0.25F)
+            .background(Color.Transparent, shape = RoundedCornerShape(16.dp))
+            .border(2.dp, Color.Transparent, RoundedCornerShape(16.dp)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "Avatar",
+            modifier = Modifier
+                .size(125.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Black, CircleShape)
+                .background(Color(0xFFADD8E6))
+        )
+
+        Text(
+            text = message,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Black,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
