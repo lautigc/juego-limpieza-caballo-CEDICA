2f34d62c3c4603ca1e0380041b88d5998dd6cf51
Franco Kumichel <francokumichel1996@gmail.com>
2025-03-01 02:36:55 -0300

    Modifico estructura de HorseParts para que tenga un atributo extra donde tendrá los poligonos para cada parte zoomeada

--- app/src/main/java/com/cedica/cedica/core/utils/HorseUtils.kt
+++ app/src/main/java/com/cedica/cedica/core/utils/HorseUtils.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.ui.geometry.Offset
 import com.cedica.cedica.R
 
-data class HorsePart(val name: String, val polygon: List<Pair<Float, Float>>, @DrawableRes val drawableRes: Int)
+data class HorsePart(val name: String, val polygon: List<Pair<Float, Float>>, val zoomedPolygon: List<Pair<Float, Float>>, @DrawableRes val drawableRes: Int)
 
 val horseParts = arrayOf(
     HorsePart("Cabeza", listOf(
@@ -14,8 +14,17 @@ val horseParts = arrayOf(
         0.0107143f to 0.2179775f,
         0.0857143f to 0.0179775f,
         0.2053571f to 0.0179775f
-    ), R.drawable.caballo_cabeza
-    ),
+    ), listOf(
+        0.06f to 0.71f,
+        0.18f to 0.91f,
+        0.39f to 0.76f,
+        0.66f to 0.71f,
+        0.95f to 0.99f,
+        0.97f to 0.45f,
+        0.43f to 0.08f,
+        0.45f to 0.33f,
+        0.24f to 0.56f
+    ), R.drawable.caballo_cabeza),
     HorsePart(name = "Cuerpo", listOf(
         0.6714286f to 0.258427f,
         0.6928571f to 0.6044944f,
@@ -23,24 +32,49 @@ val horseParts = arrayOf(
         0.4767857f to 0.611236f,
         0.4821429f to 0.4719101f,
         0.4f to 0.2674157f
-    ), R.drawable.caballo_cuerpo
-    ),
+    ), listOf(
+        0.22f to 0.12f,
+        0.33f to 0.67f,
+        0.67f to 0.62f,
+        0.71f to 0.09f,
+        0.48f to 0.15f
+    ), R.drawable.caballo_cuerpo),
     HorsePart("Cola", listOf(
         0.9767857f to 0.2831461f,
         0.9946429f to 0.4426966f,
         0.9821429f to 0.8f,
         0.9178571f to 0.8044944f,
         0.8303571f to 0.3011236f
-    ), R.drawable.caballo_cola
-    ),
+    ), listOf(
+        0.35f to 0.13f,
+        0.52f to 0.27f,
+        0.49f to 0.38f,
+        0.55f to 0.62f,
+        0.63f to 0.74f,
+        0.76f to 0.81f,
+        0.78f to 0.95f,
+        0.93f to 0.80f,
+        0.90f to 0.67f,
+        0.72f to 0.51f,
+        0.67f to 0.31f,
+        0.56f to 0.20f
+    ), R.drawable.caballo_cola),
     HorsePart("Pierna izquierda", listOf(
         0.4553571f to 0.5955056f,
         0.4928571f to 0.7820225f,
         0.4410714f to 0.9910112f,
         0.3410714f to 0.9842697f,
         0.3785714f to 0.4359551f
-    ), R.drawable.caballo_pierna_izquierda
-    ),
+    ), listOf(
+        0.48f to 0.08f,
+        0.37f to 0.19f,
+        0.48f to 0.57f,
+        0.52f to 0.79f,
+        0.58f to 0.62f,
+        0.58f to 0.46f,
+        0.69f to 0.23f,
+        0.66f to 0.13f
+    ), R.drawable.caballo_pierna_izquierda),
     HorsePart("Pierna derecha", listOf(
         0.8285714f to 0.3842697f,
         0.8785714f to 0.6898876f,
@@ -49,15 +83,34 @@ val horseParts = arrayOf(
         0.7464286f to 0.988764f,
         0.7017857f to 0.4292135f,
         0.7482143f to 0.3280899f
-    ), R.drawable.caballo_pierna_derecha
-    ),
+    ), listOf(
+        0.43f to 0.06f,
+        0.28f to 0.10f,
+        0.21f to 0.21f,
+        0.25f to 0.36f,
+        0.59f to 0.58f,
+        0.67f to 0.66f,
+        0.62f to 0.80f,
+        0.74f to 0.65f,
+        0.69f to 0.56f,
+        0.67f to 0.27f,
+        0.68f to 0.18f,
+        0.63f to 0.11f
+    ), R.drawable.caballo_pierna_derecha),
     HorsePart("Cuello", listOf(
         0.3303571f to 0.5011236f,
         0.2017857f to 0.2921348f,
         0.2732143f to 0.1820225f,
         0.3803571f to 0.2921348f
-    ), R.drawable.caballo_cuello
-    )
+    ), listOf(
+        0.19f to 0.39f,
+        0.43f to 0.59f,
+        0.73f to 0.92f,
+        0.83f to 0.67f,
+        0.80f to 0.54f,
+        0.64f to 0.53f,
+        0.29f to 0.30f
+    ),R.drawable.caballo_cuello)
 
 )
 
