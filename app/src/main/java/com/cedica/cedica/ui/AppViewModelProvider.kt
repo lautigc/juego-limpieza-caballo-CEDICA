efdf515b96fbbf9a7e6793e7d35ae7aa62bab67f
Lautaro Castro <lautaro.castro.est@gmail.com>
2025-03-11 01:25:58 -0300

    Add: Se agrega el código para la creación de los view models para alta de usuarios en el factory

--- app/src/main/java/com/cedica/cedica/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/cedica/cedica/ui/AppViewModelProvider.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.cedica.cedica.core.session.Session
 import com.cedica.cedica.data.repository.RepositoryProvider
 import com.cedica.cedica.ui.profile.ProfileListScreenViewModel
+import com.cedica.cedica.ui.profile.create.CreateTherapistFormViewModel
+import com.cedica.cedica.ui.profile.create.CreateUserFormViewModel
 import com.cedica.cedica.ui.utils.view_models.UserViewModel
 
 /**
@@ -26,6 +28,19 @@ object AppViewModelProvider {
                 userRepository = RepositoryProvider.userRepository,
                 patientRepository = RepositoryProvider.patientRepository,
                 therapistRepository = RepositoryProvider.therapistRepository,
+            )
+        }
+
+        initializer {
+            CreateUserFormViewModel(
+                userRepository = RepositoryProvider.userRepository
+            )
+        }
+
+        initializer {
+            CreateTherapistFormViewModel(
+                therapistRepository = RepositoryProvider.therapistRepository,
+                userRepository = RepositoryProvider.userRepository
             )
         }
     }
